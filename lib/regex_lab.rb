61c5e2d1e84911693c18898fda0d089944d38890
Michael Bade <mbade314@gmail.com>
2020-05-11 14:14:33 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word[0].scan(/[aeiou]/)
+  if word.scan(/[aeiou]/)
     return true
   else
     return false
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
